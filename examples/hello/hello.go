baa2e5cac63077bae6ac127b9531d525af7d4764
Hunter Loftis <hunter@hunterloftis.com>
2018-06-12 09:22:59 -0400

    update hello

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -16,9 +16,9 @@ import (
 
 func main() {
 	cam := camera.NewStandard()
-	ball := surface.UnitSphere(material.Gold(0.1)).Move(0, 0, -5)
-	floor := surface.UnitCube(material.Plastic(1, 1, 1, 0.01)).Move(0, -1, -5).Scale(100, 1, 100)
-	light := surface.UnitSphere(material.Halogen(3000)).Move(1, 0, -5).Scale(0.5, 0.5, 0.5)
+	ball := surface.UnitSphere(material.Gold(0.05)).Move(0, 0, -5)
+	floor := surface.UnitCube(material.Plastic(1, 1, 1, 0.1)).Move(0, -1, -5).Scale(100, 1, 100)
+	light := surface.UnitSphere(material.Halogen(1000)).Move(1, -0.375, -5).Scale(0.25, 0.25, 0.25)
 	surf := surface.NewList(ball, floor, light)
 	env := env.NewGradient(rgb.Black, rgb.Energy{750, 750, 750}, 7)
 	scene := render.NewScene(640, 360, cam, surf, env)
