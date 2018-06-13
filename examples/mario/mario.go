8f36f35500895ea3d79377bb35157344c659f9b9
Hunter Loftis <hunter@hunterloftis.com>
2018-06-12 21:22:39 -0400

    why does adding the floor destroy performance

--- examples/mario/mario.go
+++ examples/mario/mario.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hunterloftis/pbr2/pkg/env"
 	"github.com/hunterloftis/pbr2/pkg/formats/obj"
 	"github.com/hunterloftis/pbr2/pkg/geom"
+	"github.com/hunterloftis/pbr2/pkg/material"
 	"github.com/hunterloftis/pbr2/pkg/render"
 	"github.com/hunterloftis/pbr2/pkg/rgb"
 	"github.com/hunterloftis/pbr2/pkg/surface"
@@ -26,9 +27,9 @@ func main() {
 	flag.Parse()
 
 	// light := material.Light(1700, 1700, 1700)
-	// whitePlastic := material.Plastic(1, 1, 1, 0.07)
-	// bluePlastic := material.Plastic(0, 0, 1, 0.01)
-	// grid := material.NewGrid(whitePlastic, bluePlastic, 20000, 0.1)
+	whitePlastic := material.Plastic(1, 1, 1, 0.07)
+	bluePlastic := material.Plastic(0, 0, 1, 0.01)
+	grid := material.NewGrid(whitePlastic, bluePlastic, 20000, 0.1)
 
 	mario, err := obj.ReadFile("fixtures/models/mario/mario-sculpture.obj", true)
 	if err != nil {
@@ -38,11 +39,12 @@ func main() {
 	// sky := env.NewFlat(40, 50, 60)
 	sky := env.NewGradient(rgb.Black, rgb.Energy{2000, 2000, 2000}, 4)
 	cam := camera.NewStandard().MoveTo(0, 0, 300).LookAt(geom.Vec{}, geom.Vec{0, 0, 0})
-	// floor := surface.UnitCube(grid).Move(0, -0.55, 0).Scale(1000, 1, 1000)
+	floor := surface.UnitCube(grid).Move(0, -32, 0).Scale(1000, 1, 1000)
 	// lamp := surface.UnitSphere(light).Move(7, 30, 6).Scale(30, 30, 30)
 
 	// objects := append(mario.SurfaceObjects(), floor, lamp)
-	tree := surface.NewTree(mario.SurfaceObjects()...)
+	objects := append(mario.SurfaceObjects(), floor)
+	tree := surface.NewTree(objects...)
 	scene := render.NewScene(888, 600, cam, tree, sky)
 	frame := render.NewFrame(scene)
 
