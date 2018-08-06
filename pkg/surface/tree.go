b70c0f648b09339ab6985bba89c5f21862b5a2f1
Hunter Loftis <hunter@hunterloftis.com>
2018-08-06 16:25:36 -0400

    return to simple cycling axis, median-split tree structure

--- pkg/surface/tree.go
+++ pkg/surface/tree.go
@@ -1,23 +1,16 @@
 package surface
 
 import (
-	"math"
+	"sort"
 
 	"github.com/hunterloftis/pbr2/pkg/geom"
 	"github.com/hunterloftis/pbr2/pkg/render"
 )
 
 const (
-	xAxis        = 0
-	yAxis        = 1
-	zAxis        = 2
-	traversal    = 1.0
-	intersection = 8.0
-	bins         = 5
-	minContents  = 8
+	minContents = 5
 )
 
-// TODO: concurrent construction
 type Tree struct {
 	branch
 	lights []render.Object
@@ -34,8 +27,7 @@ type branch struct {
 }
 
 func NewTree(ss ...render.Surface) *Tree {
-	maxDepth := int(5 * math.Round(8+1.3*math.Log2(float64(len(ss))))) // PBRT "acceleration structures" chapter
-	// maxDepth := int(math.Round(8 + 1.3*math.Log2(float64(len(ss))))) // PBRT "acceleration structures" chapter
+	maxDepth := 12
 	t := Tree{
 		branch: *newBranch(boundsAround(ss), ss, maxDepth),
 	}
@@ -62,15 +54,19 @@ func newBranch(bounds *geom.Bounds, surfaces []render.Surface, depth int) *branc
 		b.leaf = true
 		return &b
 	}
-	axis, wall, ok := split(b.surfaces, b.bounds)
-	if !ok {
-		b.leaf = true
-		return &b
+	b.axis = 0
+	max := -1.0
+	for a := 0; a < 3; a++ {
+		dist := bounds.Max.Axis(a) - bounds.Min.Axis(a)
+		if dist > max {
+			max = dist
+			b.axis = a
+		}
 	}
-	b.axis, b.wall = axis, wall
-	lbounds, rbounds := bounds.Split(b.axis, b.wall)
-	b.left = newBranch(lbounds, b.surfaces, depth-1)
-	b.right = newBranch(rbounds, b.surfaces, depth-1)
+	b.wall = median(b.surfaces, b.axis)
+	lBounds, rBounds := bounds.Split(b.axis, b.wall)
+	b.left = newBranch(lBounds, b.surfaces, depth-1)
+	b.right = newBranch(rBounds, b.surfaces, depth-1)
 	return &b
 }
 
@@ -112,52 +108,6 @@ func (b *branch) IntersectSurfaces(r *geom.Ray, max float64) (obj render.Object,
 	return obj, dist
 }
 
-// Compare the cost of not splitting against splitting at 7 different points along each axis
-func split(ss []render.Surface, bounds *geom.Bounds) (axis int, wall float64, ok bool) {
-	axis, min, max := extents(ss)
-	stride := (max - min) / bins
-	lb, rb := bounds.Split(axis, stride)
-	wall, cost := min+stride, sah(ss, lb, rb)
-	for w := min + stride*2; w < max-bias; w += stride {
-		lb, rb := bounds.Split(axis, w)
-		if c := sah(ss, lb, rb); c < cost {
-			wall, cost = w, c
-		}
-	}
-	baseCost := bounds.SurfaceArea() * float64(len(ss)) * intersection
-	if baseCost <= cost {
-		return 0, 0, false
-	}
-	return axis, wall, true
-}
-
-// Surface Are Heuristic
-// https://medium.com/@bromanz/how-to-create-awesome-accelerators-the-surface-area-heuristic-e14b5dec6160
-func sah(ss []render.Surface, aBounds, bBounds *geom.Bounds) float64 {
-	aSurfaces := overlaps(aBounds, ss)
-	bSurfaces := overlaps(bBounds, ss)
-	a := aBounds.SurfaceArea() * float64(len(aSurfaces)) * intersection
-	b := bBounds.SurfaceArea() * float64(len(bSurfaces)) * intersection
-	return traversal + a + b
-}
-
-func extents(ss []render.Surface) (axis int, low, high float64) {
-	min := geom.Vec{math.Inf(1), math.Inf(1), math.Inf(1)}
-	max := geom.Vec{math.Inf(-1), math.Inf(-1), math.Inf(-1)}
-	for _, s := range ss {
-		c := s.Bounds().Center
-		min = c.Min(min)
-		max = c.Max(max)
-	}
-	span := max.Minus(min).Abs()
-	if span.X > span.Y && span.X > span.Z {
-		return xAxis, min.X, max.X
-	} else if span.Y > span.X && span.Y > span.Z {
-		return yAxis, min.Y, max.Y
-	}
-	return zAxis, min.Z, max.Z
-}
-
 func overlaps(bounds *geom.Bounds, surfaces []render.Surface) []render.Surface {
 	matches := make([]render.Surface, 0)
 	for _, s := range surfaces {
@@ -166,4 +116,14 @@ func overlaps(bounds *geom.Bounds, surfaces []render.Surface) []render.Surface {
 		}
 	}
 	return matches
+}
+
+func median(surfaces []render.Surface, axis int) float64 {
+	vals := make([]float64, 0)
+	for _, s := range surfaces {
+		b := s.Bounds()
+		vals = append(vals, b.MinArray[axis], b.MaxArray[axis])
+	}
+	sort.Float64s(vals)
+	return vals[len(vals)/2]
 }
