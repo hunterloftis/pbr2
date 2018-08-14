43b326dc45bc682297d07b617bbc0d5ef11e0fe3
Hunter Loftis <hunter@hunterloftis.com>
2018-08-13 23:51:06 -0400

    big gopher

--- examples/toys/toys.go
+++ examples/toys/toys.go
@@ -53,7 +53,7 @@ func run() error {
 	camera.FStop = 1.4
 
 	table.Scale(geom.Vec{37, 37, 37}).Rotate(geom.Vec{0, math.Pi * 0.5, 0}).MoveTo(geom.Vec{0, 0, -2}, geom.Vec{0, 1, 0})
-	gopher.Scale(geom.Vec{0.5, 0.5, 0.5}).Rotate(geom.Vec{0, -2, 0}).MoveTo(geom.Vec{0.05, 0, 0.1}, geom.Vec{0, -1, 0})
+	gopher.Scale(geom.Vec{0.6, 0.6, 0.6}).Rotate(geom.Vec{0, -2, 0}).MoveTo(geom.Vec{0.05, 0, 0.1}, geom.Vec{0, -1, 0})
 	mario.Scale(geom.Vec{0.005, 0.005, 0.005}).MoveTo(geom.Vec{-0.3, 0, -0.2}, geom.Vec{0, -1, 0})
 	buddha.Scale(geom.Vec{0.9, 0.9, 0.9}).Rotate(geom.Vec{0, math.Pi, 0}).MoveTo(geom.Vec{0.6, 0, 0.5}, geom.Vec{0, -1, 0})
 	lego.Scale(geom.Vec{0.003, 0.003, 0.003}).Rotate(geom.Vec{0, 0.08, 0}).MoveTo(geom.Vec{0.9, 0, -0.6}, geom.Vec{0, -1, 0})
@@ -67,8 +67,8 @@ func run() error {
 
 	mats := []surface.Material{
 		material.Glass(0.3),
-		material.Plastic(0.31, 0.8, 0.77, 0.3),
-		material.Plastic(0.73, 0.67, 0.76, 0.3),
+		material.Plastic(0.31, 0.8, 0.77, 0.15),
+		material.Plastic(0.73, 0.67, 0.76, 0.15),
 		material.Mirror(0.15),
 	}
 	rand.Seed(17)
