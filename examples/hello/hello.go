aacc3b2f3fe32979fc6fce7d15689397e77bdd37
Hunter Loftis <hunter@hunterloftis.com>
2018-08-17 16:16:18 -0400

    simpler hello world

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -14,16 +14,16 @@ import (
 )
 
 func main() {
-	env := env.NewGradient(rgb.Black, rgb.Energy{750, 750, 750}, 7)
 	floor := surface.UnitCube(material.Plastic(1, 1, 1, 0.05))
 	floor.Shift(geom.Vec{0, -0.1, 0}).Scale(geom.Vec{10, 0.1, 10})
 	ball := surface.UnitSphere(material.Gold(0.05, 1))
 	ball.Scale(geom.Vec{0.1, 0.1, 0.1})
-	cam := camera.NewSLR()
-	cam.MoveTo(geom.Vec{0, 0, -0.5}).LookAt(geom.Vec{0, 0, 0})
 
-	surf := surface.NewList(ball, floor)
-	scene := render.NewScene(cam, surf, env)
+	c := camera.NewSLR().MoveTo(geom.Vec{0, 0, -0.5}).LookAt(geom.Vec{0, 0, 0})
+	s := surface.NewList(ball, floor)
+	e := env.NewGradient(rgb.Black, rgb.Energy{750, 750, 750}, 7)
+
+	scene := render.NewScene(c, s, e)
 	err := render.Iterative(scene, "hello.png", 898, 450, 8, true)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "\nError: %v\n", err)
